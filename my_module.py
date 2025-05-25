cf75d0a7aff336e6c557307f47e63a0100044473
tope0607 <tope0607@gmail.com>
2025-05-25 14:02:28 +0100

    Create my_module.py

---
+++ my_module.py
@@ -0,0 +1,7 @@
+def reverse_text(text):
+    """Returns the reversed version of the input text."""
+    return text[::-1]
+
+def add_numbers(a, b):
+    """Returns the sum of two numbers."""
+    return a + b
